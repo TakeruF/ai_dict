8def72f79395ce652ae97a62c6797d92ed9f6f4a
TakeruF <99144975+TakeruF@users.noreply.github.com>
2026-02-28 21:25:58 +0900

    release: beta apk

---
+++ android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aidict.app',
+  appName: 'AI Dict',
+  webDir: 'out'
+};
+
+export default config;
